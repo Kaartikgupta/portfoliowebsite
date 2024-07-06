c9c3771e6f5e49d62c50fab090dee0c91447f4e0
Kaartikgupta <kaartik1999@gmail.com>
2024-07-06 22:09:13 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Kaartik's portfolio coming soon in near future....
+        Kaartik's portfolio coming soon in near future, started building it
       </header>
     </div>
   );
